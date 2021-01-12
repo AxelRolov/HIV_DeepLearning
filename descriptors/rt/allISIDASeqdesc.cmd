1dcfbe8bdd53bb8dd9e1f6888300815cab18e5f6
AxelRolov <data2scale@gmail.com>
2021-01-12 08:31:54 +0100

    First update with my files

---
+++ descriptors/rt/allISIDASeqdesc.cmd
@@ -0,0 +1,14 @@
+#!/bin/bash
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO33S3F -t 1 -l 3 -u 3 -s 3 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO13S1F -t 1 -l 1 -u 3 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO23S1F -t 1 -l 2 -u 3 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO33S1F -t 1 -l 3 -u 3 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO44S1F -t 1 -l 4 -u 4 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO55S1F -t 1 -l 5 -u 5 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO55S1 -t 1 -l 5 -u 5 -s 1
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO44S1 -t 1 -l 4 -u 4 -s 1
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO13S1 -t 1 -l 1 -u 3 -s 1
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIO33S3 -t 1 -l 3 -u 3 -s 1
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIN13S1F -t 3 -l 1 -u 3 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIN23S1F -t 3 -l 2 -u 3 -s 1 -p 2
+python /mnt/c//Users/Alexis/PycharmProjects/ISIDA-Seq/src/ISIDASeq.py -i rt.seq -o PIN13S1 -t 1 -l 1 -u 3 -s 1
